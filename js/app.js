faa4a05eba85775447b3cc9e320e3e10781cf2e3
martapz <marta.g.mattioli@gmail.com>
2017-07-01 18:09:28 +0100

    added more js

--- js/app.js
+++ js/app.js
@@ -5,20 +5,35 @@
 
 //Start intervals when the start button is clicked
 var progressColorCounter = 0;
-var colors = ['red', 'yellow', 'green', 'blue', 'pink'];
+var colors = ['red', 'yellow'];
 var dotColors;
 var randomIntervals = Math.floor(Math.random() * 10000) + 2000;
 
 //Bubbles positioning
-var xCoordinate = `${Math.floor(Math.random() * 96)}%`;
-var yCoordinate = `${Math.floor(Math.random() * 96)}%`;
+// !!!!!!! Not sure why, but sometimes it gives me undefined %
+var xCoordinate = `${xPositionExclude50()}%`;
+var yCoordinate = `${yPositionExclude50()}%`;
 
-// function exclude50() {
-//   var num = Math.floor(Math.random() * (max - min + 1)) + min;
-//   return (num === 8 || num === 15) ? generateRandom(min, max) : num;
-// }
+function xPositionExclude50() {
+  xCoordinate = Math.floor(Math.random() * 93) + 3;
+  if (xCoordinate >= 47 && xCoordinate <= 53) {
+    yPositionExclude50();
+  } else {
+    return xCoordinate;
+  }
+}
 
-var bubbleColors;
+function yPositionExclude50() {
+  yCoordinate = Math.floor(Math.random() * 93) + 3;
+  if (yCoordinate >= 47 && yCoordinate <= 53) {
+    yPositionExclude50();
+  } else {
+    return yCoordinate;
+  }
+}
+
+var bubbleIntervals;
+var bubbleIds = 0;
 
 function startDotInterval() {
   dotColors = setInterval(changeDotColor, randomIntervals);
@@ -29,7 +44,7 @@ function changeDotColor() {
   $('.center-dot').css('background', colors[progressColorCounter]).attr('value', colors[progressColorCounter]);
   console.log(progressColorCounter);
   //Progress colors index at random
-  progressColorCounter = Math.floor(Math.random() * (colors.length - 1));
+  progressColorCounter = Math.floor(Math.random() * (colors.length));
   //Reassign a random number between 2 seconds and 12 seconds
   randomIntervals = Math.floor(Math.random() * 10000) + 2000;
 }
@@ -37,23 +52,33 @@ function changeDotColor() {
 //Make bubbles appear on the screen on set intervals
 // !!!!!!!!!!!! MAKE THE 2000 TURN INTO INCREASING SPEED OF FILL UP
 function appearBubbles() {
-  bubbleColors = setInterval(addBubbles, 2000);
+  bubbleIntervals = setInterval(addBubbles, 2000);
+}
+
+var bubbleColor = colors[Math.floor(Math.random() * (colors.length))];
+function createBubble() {
+
+}
+
+function disappearBubbles(e) {
+  console.log(e.target);
+  // $(e.target).remove();
 }
 
 function addBubbles() {
-  var bubbleColor = colors[Math.floor(Math.random() * (colors.length - 1))];
-  $('<div/>').addClass('bubble').appendTo('.game-board').css({
+  var bubbleColor = colors[Math.floor(Math.random() * (colors.length))];
+  $('<div/>').addClass('bubble').attr('id', `${bubbleIds}`).attr('value', `${bubbleColor}`).appendTo('.game-board').css({
     'left': xCoordinate,
     'top': yCoordinate,
     'background-color': bubbleColor
   });
-  $('.bubble').attr('value', `${bubbleColor}`);
-  xCoordinate = `${Math.floor(Math.random() * 96)}%`;
-  yCoordinate = `${Math.floor(Math.random() * 96)}%`;
+  bubbleIds++;
+  xCoordinate = `${xPositionExclude50()}%`;
+  yCoordinate = `${yPositionExclude50()}%`;
+  setTimeout(disappearBubbles, 2000);
 }
 
 function init() {
-  console.log(xCoordinate, yCoordinate);
   // When user clicks START, the button disappears
   // In place there is a circle in the middle
   $('.start-button').on('click', () => {
