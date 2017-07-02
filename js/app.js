34b77339724be9cca5e104ba3d7fc23d28fe34db
martapz <marta.g.mattioli@gmail.com>
2017-07-02 10:15:19 +0100

    added more game over actions

--- js/app.js
+++ js/app.js
@@ -13,21 +13,23 @@ var colors = ['red', 'yellow', 'green'];
 //Variables for the central dot
 var progressColorCounter = 0;
 var dotColors;
-var randomIntervals = Math.floor(Math.random() * 10000) + 2000;
+var randomIntervals = Math.floor(Math.random() * (10000 - 2000 + 1)) + 2000;
 
 //Bubbles positioning
 // !!!!!!! Not sure why, but sometimes it gives me undefined %
+var paddingNow;
 var xCoordinate = `${xPositionExclude50()}%`;
 var yCoordinate = `${yPositionExclude50()}%`;
 
 //Bubble intervals variables
 var bubbleIntervals;
+var randomFreq = Math.floor((Math.random() * (3000 - 1000 + 1)) + 1000);
 var bubbleIds = 0;
 var timerIds = 0;
 var removeDiv = 0;
 
 function xPositionExclude50() {
-  xCoordinate = Math.floor(Math.random() * 93) + 3;
+  xCoordinate = Math.floor((Math.random() * (80 - 10 + 1)) + 20);
   if (xCoordinate >= 47 && xCoordinate <= 53) {
     yPositionExclude50();
   } else {
@@ -36,7 +38,7 @@ function xPositionExclude50() {
 }
 
 function yPositionExclude50() {
-  yCoordinate = Math.floor(Math.random() * 93) + 3;
+  yCoordinate = Math.floor((Math.random() * (80 - 10 + 1)) + 20);
   if (yCoordinate >= 47 && yCoordinate <= 53) {
     yPositionExclude50();
   } else {
@@ -54,50 +56,86 @@ function changeDotColor() {
   //Progress colors index at random
   progressColorCounter = Math.floor(Math.random() * (colors.length));
   //Reassign a random number between 2 seconds and 12 seconds
-  randomIntervals = Math.floor(Math.random() * 10000) + 2000;
+  randomIntervals = Math.floor((Math.random() * (10000 - 2000 + 1)) + 2000);
 }
 
 //Intervals for various bubbles
 //Make bubbles appear on the screen on set intervals
 // !!!!!!!!!!!! MAKE THE 2000 TURN INTO INCREASING SPEED OF FILL UP
 function appearBubbles() {
-  bubbleIntervals = setInterval(addBubbles, 2000);
+  bubbleIntervals = setInterval(addBubbles, randomFreq);
 }
 
 function addBubbles() {
+  // var xCoordinate = `${xPositionExclude50()}%`;
+  // var yCoordinate = `${yPositionExclude50()}%`;
+  //
+  // function xPositionExclude50() {
+  //   xCoordinate = Math.floor((Math.random() * ((100 - paddingNow) - paddingNow + 1)) + paddingNow);
+  //   if (xCoordinate >= 47 && xCoordinate <= 53) {
+  //     yPositionExclude50();
+  //   } else {
+  //     return xCoordinate;
+  //   }
+  // }
+  //
+  // function yPositionExclude50() {
+  //   yCoordinate = Math.floor((Math.random() * ((100 - paddingNow) - paddingNow + 1)) + paddingNow);
+  //   if (yCoordinate >= 47 && yCoordinate <= 53) {
+  //     yPositionExclude50();
+  //   } else {
+  //     return yCoordinate;
+  //   }
+  // }
+
   var bubbleColor = colors[Math.floor(Math.random() * (colors.length))];
-  $('<div/>').addClass('bubble').attr('id', `${bubbleIds}`).attr('value', `${bubbleColor}`).appendTo('.game-board').css({
+  $('<div/>').addClass('bubble').attr('id', `${bubbleIds}`).attr('value', `${bubbleColor}`).fadeIn(3000).appendTo('.game-board').css({
     'left': xCoordinate,
     'top': yCoordinate,
     'background-color': bubbleColor,
-    'padding': Math.floor(Math.random() * 30) + 10
+    'padding': Math.floor((Math.random() * (30 - 10 + 1)) + 10)
   });
+  //!!!!!!! SOMETIMES XCOORDINATE AND Y COORDINATE ARE UNDEFINED... WHY??
+  // console.log(xCoordinate, yCoordinate);
+
+  //!!!!!! with paddingNow, I'm trying to make sure that the cicles don't go out of the light grey box -->
+  //the problem now is that at some point something break, but I'm not sure what... the console logs start to go mental
+  // console.log(`Log padding: ${parseFloat($(`#${bubbleIds}`).css('padding').slice(0, -2))}`);
+  // console.log(`Log posX: ${xPositionExclude50()}%; Log posY: ${yPositionExclude50()}%;`);
+  // var getBubblepadding = parseFloat($(`#${bubbleIds}`).css('padding').slice(0, -2));
+  // paddingNow = getBubblepadding * 2;
+
   xCoordinate = `${xPositionExclude50()}%`;
   yCoordinate = `${yPositionExclude50()}%`;
+
   //Fade out and remove divs
   setTimeout(function() {
-    $(`#${timerIds}`).fadeOut();
+    $(`#${timerIds}`).fadeOut(3000);
     setTimeout( function() {
       $(`#${removeDiv}`).remove();
       removeDiv++;
-    }, 1000);
+    }, 3000);
     timerIds++;
   }, 3000);
   bubbleIds++;
+
   //Add event listener to IDs --> if value of center-dot === value of clicked item, success // else game over
   $('.bubble').one('click', checkColor);
-}
 
+  //Give a new random number between 1 and 3 seconds to the frequency of bubble appearance
+  randomFreq = Math.floor(Math.random() * (3000 - 1000 + 1)) + 1000;
+}
 
 function checkColor(e) {
   if ($(e.target).attr('value') === $('.center-dot').attr('value')) {
     console.log(`Clicked value: ${$(e.target).attr('value')}`, `Center Dot: ${$('.center-dot').attr('value')}`, 'YAY');
     score++;
+    console.log(score);
     $('#score').html(score);
     $(e.target).remove();
   } else {
     console.log(`Clicked value: ${$(e.target).attr('value')}`, `Center Dot: ${$('.center-dot').attr('value')}`, 'OH NO');
-    $(`#life-${lives}`).remove();
+    $(`#life-${lives}`).fadeOut();
     lives--;
     $(e.target).remove();
     if (lives === 0) {
@@ -106,12 +144,32 @@ function checkColor(e) {
       highScore.sort(function(a, b) {
         return a-b;
       });
+      //display high score - do not reset it
       $('#highest-score').html(highScore[highScore.length - 1]);
+      //reset lives to 3 and make hearts appear again
+      lives = 3;
+      $(`.lives`).fadeIn();
+      //reset score to 0
+      score = 0;
+      //Stop intervals
+      
+      //Make board disappear and make game over message appear
+      $('.game-board').fadeOut('slow');
+      $('.game-over').fadeIn('slow');
+      $('.play-again').on('click', () => {
+        //Make game over message disappear and game board appear
+
+        //Reactivate intervals for bubbles and center-dot
+
+      });
     }
   }
 }
 
 function init() {
+  // for (var i = 0; i < 100; i++) {
+  //   console.log(Math.floor((Math.random() * (80 - 10 + 1)) + 10));
+  // }
   // When user clicks START, the button disappears
   // In place there is a circle in the middle
   $('.start-button').on('click', () => {
