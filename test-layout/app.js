0bef0b55218ac8527a38e2bb5b97560070fc3e94
martapz <marta.g.mattioli@gmail.com>
2017-07-04 15:57:15 +0100

    testing layout

--- test-layout/app.js
+++ test-layout/app.js
@@ -8,32 +8,37 @@ var otherOptHeight = bodyHeight * 0.15;
 function init() {
   var getInstructionsDiv = $('.instructions');
   var getInstructionsHeight = getInstructionsDiv.height();
-  // console.log(getInstructionsHeight);
-//When page loads
 
 //After initial animation
   //Main container
   $('.container').height(viewportHeight);
-
   //Title Area
   $('header').height(headerHeight).css('padding', `${(headerHeight - 30) / 2}`);
-
   //Main
   $('main').height(viewportHeight * 0.9);
-
   //Start Area
   $('.start-area').height(bodyHeight).css('padding', `${startAreaPadding}`);
-
   //Game Area
   $('.game-area').height(bodyHeight);
-
   //Game board
   $('.scoring-area').height(scoringAreaHeight).css('padding', `${(scoringAreaHeight - parseFloat($('h3:first-child').css('font-size'))) / 2}`);
   $('.game-board').height(bodyHeight * 0.65);
   $('.other-options').height(otherOptHeight).css('padding', `${(otherOptHeight - 51) / 2}`);
-
   //Instructions
   $('.instructions-area').height(bodyHeight).css('padding', `${(bodyHeight - 25 - getInstructionsHeight - 51) / 2}`);
+
+  let counter = 0;
+  const $spanArray = $('h1 span');
+  const titleInterval = setInterval(function() {
+    $($spanArray[counter]).addClass('animated bounce');
+
+    counter++;
+
+    if (counter === $spanArray.length) {
+      clearInterval(titleInterval);
+    }
+  }, 200);
+
 }
 
 $(init);
