676949be2ed1d8188d54609d6a34172f4f613ce8
martapz <marta.g.mattioli@gmail.com>
2017-07-04 13:08:43 +0100

    testing layout

---
+++ test-layout/app.js
@@ -0,0 +1,39 @@
+var viewportHeight = $(window).height();
+var headerHeight = viewportHeight * 0.1;
+var bodyHeight = viewportHeight * 0.9;
+var startAreaPadding = (bodyHeight - 91) / 2;
+var scoringAreaHeight = bodyHeight * 0.1;
+var otherOptHeight = bodyHeight * 0.15;
+
+function init() {
+  var getInstructionsDiv = $('.instructions');
+  var getInstructionsHeight = getInstructionsDiv.height();
+  // console.log(getInstructionsHeight);
+//When page loads
+
+//After initial animation
+  //Main container
+  $('.container').height(viewportHeight);
+
+  //Title Area
+  $('header').height(headerHeight).css('padding', `${(headerHeight - 30) / 2}`);
+
+  //Main
+  $('main').height(viewportHeight * 0.9);
+
+  //Start Area
+  $('.start-area').height(bodyHeight).css('padding', `${startAreaPadding}`);
+
+  //Game Area
+  $('.game-area').height(bodyHeight);
+
+  //Game board
+  $('.scoring-area').height(scoringAreaHeight).css('padding', `${(scoringAreaHeight - parseFloat($('h3:first-child').css('font-size'))) / 2}`);
+  $('.game-board').height(bodyHeight * 0.65);
+  $('.other-options').height(otherOptHeight).css('padding', `${(otherOptHeight - 51) / 2}`);
+
+  //Instructions
+  $('.instructions-area').height(bodyHeight).css('padding', `${(bodyHeight - 25 - getInstructionsHeight - 51) / 2}`);
+}
+
+$(init);
